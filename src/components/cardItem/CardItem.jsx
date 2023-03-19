d46bb5855d95245caf37dd3de0ce77c0e762944e
ShecktorS <ettoresanfilippo@gmail.com>
2023-03-19 18:29:10 +0100

    cycled list

--- src/components/cardItem/CardItem.jsx
+++ src/components/cardItem/CardItem.jsx
@@ -1,10 +1,10 @@
 import "./index.css";
 
-const CardItem = () => {
+const CardItem = ({ item }) => {
   return (
     <div className="CardItem">
       <div className="CardItem__image">
-        <img src={products.thumbnail} alt={products.title} />
+        <img src={item.thumbnail} alt={item.title} />
         <div
           className="likeButton"
           onClick={() =>
@@ -15,12 +15,12 @@ const CardItem = () => {
         </div>
       </div>
       <div className="CardItem__info">
-        <h3 className="CardItem__info--title">{products.title}</h3>
+        <abbr title={item.title}>
+          <h3 className="CardItem__info--title">{item.title.slice(0, 12)}</h3>
+        </abbr>
         <div className="CardItem_info--downSection">
-          <p className="CardItem_info--price">€ {products.price},00</p>
-          <p className="CardItem_info--rating">
-            ⭐ {products.rating.toFixed(1)}
-          </p>
+          <p className="CardItem_info--price">€ {item.price},00</p>
+          <p className="CardItem_info--rating">⭐ {item.rating.toFixed(1)}</p>
           <div className="addToCart">+</div>
         </div>
       </div>
@@ -30,22 +30,22 @@ const CardItem = () => {
 
 export default CardItem;
 
-const products = {
-  id: 1,
-  title: "iPhone 9",
-  description: "An apple mobile which is nothing like apple",
-  price: 549,
-  discountPercentage: 12.96,
-  rating: 4.69,
-  stock: 94,
-  brand: "Apple",
-  category: "smartphones",
-  thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-  images: [
-    "https://i.dummyjson.com/data/products/1/1.jpg",
-    "https://i.dummyjson.com/data/products/1/2.jpg",
-    "https://i.dummyjson.com/data/products/1/3.jpg",
-    "https://i.dummyjson.com/data/products/1/4.jpg",
-    "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-  ],
-};
+// const products = {
+//   id: 1,
+//   title: "iPhone 9",
+//   description: "An apple mobile which is nothing like apple",
+//   price: 549,
+//   discountPercentage: 12.96,
+//   rating: 4.69,
+//   stock: 94,
+//   brand: "Apple",
+//   category: "smartphones",
+//   thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+//   images: [
+//     "https://i.dummyjson.com/data/products/1/1.jpg",
+//     "https://i.dummyjson.com/data/products/1/2.jpg",
+//     "https://i.dummyjson.com/data/products/1/3.jpg",
+//     "https://i.dummyjson.com/data/products/1/4.jpg",
+//     "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+//   ],
+// };
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,11 +1,16 @@
 import "./index.css";
 import CardItem from "../cardItem";
+import { products } from "../../mock/products";
 
 const CardList = ({ titleText }) => {
   return (
     <div className="CardList">
       <h2>{titleText}</h2>
-      <CardItem />
+      <div className="CardList__content">
+        {products.map((item) => (
+          <CardItem item={item} />
+        ))}
+      </div>
     </div>
   );
 };
